91bd995b04c9e7b93b4d0efca9cb484966fcec2c
Mills24 <55164462+Mills24@users.noreply.github.com>
2019-12-08 18:10:44 -0600

    Save3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product.component';
 import { StarComponent } from './shared/star.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    StarComponent
+    StarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path : 'products', component : ProductListComponent},
+      {path : 'welcome', component : WelcomeComponent},
+      {path : '', redirectTo : 'welcome', pathMatch : 'full'},
+      {path : '**', redirectTo : 'welcome', pathMatch : 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
